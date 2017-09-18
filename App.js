5fc83ebfc262bd93437bd241ea5c8db020332ff9
Ankur Kedia <ankur@geekyants.com>
2017-09-18 16:09:53 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,2 @@
+import App from "./build/Root";
+export default App;
---
+++ build/Root.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// import React from 'react';
+const React = require("react");
+const react_native_1 = require("react-native");
+class App extends React.Component {
+    render() {
+        return (<react_native_1.View style={styles.container}>
+        <react_native_1.Text>Open up App.js to start working on your app!</react_native_1.Text>
+        <react_native_1.Text>Changes you make will automatically reload.</react_native_1.Text>
+        <react_native_1.Text>Shake your phone to open the developer menu.</react_native_1.Text>
+      </react_native_1.View>);
+    }
+}
+exports.default = App;
+const styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+//# sourceMappingURL=Root.js.map
